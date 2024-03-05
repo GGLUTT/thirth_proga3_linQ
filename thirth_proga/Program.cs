36060b68f7606e283e82bc1ac9a23cb22a560cfe
Zhenya Luter <newgglutt@gmail.com>
2024-03-05 20:40:02 +0200

    Add project files.

---
+++ thirth_proga/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        List<int> numbers = new List<int> { 4, 1, 3, 2, 9, 8, 7, 5, 6, 3, 1, 5 };
+
+        
+        var distinctOdds = numbers.Where(x => x % 2 != 0) // Вибираємо непарні числа
+                                   .GroupBy(x => x)        // Групуємо їх по значенню
+                                   .Select(g => g.First()) // Беремо перший елемент з кожної групи
+                                   .ToList();              // Конвертуємо у список
+
+
+        Console.WriteLine("Всі непарні числа, з видаленням дублікатів:");
+        distinctOdds.ForEach(Console.WriteLine);
+    }
+}
